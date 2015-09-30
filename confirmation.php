d774d05726704d33c794fd8ec64085ee17fe7a9a
yasseraboelsaad <yasser.aboelsaad@gmail.com>
2015-09-30 23:08:10 +0200

    updated html and css

--- confirmation.php
+++ confirmation.php
@@ -1,1 +1,21 @@
-<title>Confirmation</title>
+<!DOCTYPE html>
+<html>
+<head>
+		<title>
+			Checkout
+		</title>
+		<link rel="stylesheet" type="text/css" href="stylesheet.css">
+	</head>
+<body>
+	<div id="topbar">
+		<table style="width:100%">
+			<tr>
+		    	<td><img src="shoppingcart.png" width = "50" height = "40"> eShop</td>
+		    	<td>Orders</td>
+		    	<td>logout</td>
+		   	</tr>
+		</table>
+	</div>
+	<h1>Your order was successfully purchased.</h1>
+</body>
+</html>
--- signup.php
+++ signup.php
@@ -1,69 +1,86 @@
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-12">
-			<form class="form-horizontal" role="form">
-				<div class="form-group">
-					 
-					<label for="inputFname3" class="col-sm-2 control-label">
-						First Name
-					</label>
-					<div class="col-sm-10">
-						<input type="fname" class="form-control" id="inputEmail3" />
-					</div>
-				</div>
-				<label for="inputLname3" class="col-sm-2 control-label">
-						Last Name
-					</label>
-					<div class="col-sm-10">
-						<input type="lname" class="form-control" id="inputEmail3" />
-					</div>
-				</div>
-					<div class="form-group">
-					 <label for="inputEmail3" class="col-sm-2 control-label">
-						Email
-					</label>
-					<div class="col-sm-10">
-						<input type="email" class="form-control" id="inputEmail3" />
-					</div>
-					<div class="form-group">
-					 
-					<label for="inputPassword3" class="col-sm-2 control-label">
-						Password
-					</label>
-					<div class="col-sm-10">
-						<input type="password" class="form-control" id="inputPassword3" />
-					</div>
-					<div class="form-group">
-					 
-					<label for="inputCPassword3" class="col-sm-2 control-label">
-						Confirm Password
-					</label>
-					<div class="col-sm-10">
-						<input type="Cpassword" class="form-control" id="inputPassword3" />
-					</div>
-					<div class="form-group">
-					 
-					<label for="inputAvatar3" class="col-sm-2 control-label">
-						Avatar
-					</label>
-					<div class="col-sm-offset-2 col-sm-10">
-						 
-						<button type="browse" class="btn btn-default">
-							Browse
-						</button>
-					</div>
-				</div>
-				</div>
-				</div>
-				<div class="form-group">
-					<div class="col-sm-offset-2 col-sm-10">
-						 
-						<button type="submit" class="btn btn-default">
-							Sign up
-						</button>
-					</div>
-				</div>
-			</form>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			Sign up
+		</title>
+		<link rel="stylesheet" type="text/css" href="stylesheet.css">
+	</head>
+	<body>
+		<div id="topbar">
+			<table style="width:100%">
+				<tr>
+			    	<td><img src="shoppingcart.png" width = "50" height = "40"> eShop</td>
+			   	</tr>
+			</table>
 		</div>
-	</div>
-</div>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-12">
+					<form class="form-horizontal" role="form">
+						<div class="form-group">
+							 
+							<label for="inputFname3" class="col-sm-2 control-label">
+								First Name
+							</label>
+							<div class="col-sm-10">
+								<input type="fname" class="form-control" id="inputEmail3" />
+							</div>
+						</div>
+						<label for="inputLname3" class="col-sm-2 control-label">
+								Last Name
+							</label>
+							<div class="col-sm-10">
+								<input type="lname" class="form-control" id="inputEmail3" />
+							</div>
+						</div>
+							<div class="form-group">
+							 <label for="inputEmail3" class="col-sm-2 control-label">
+								Email
+							</label>
+							<div class="col-sm-10">
+								<input type="email" class="form-control" id="inputEmail3" />
+							</div>
+							<div class="form-group">
+							 
+							<label for="inputPassword3" class="col-sm-2 control-label">
+								Password
+							</label>
+							<div class="col-sm-10">
+								<input type="password" class="form-control" id="inputPassword3" />
+							</div>
+							<div class="form-group">
+							 
+							<label for="inputCPassword3" class="col-sm-2 control-label">
+								Confirm Password
+							</label>
+							<div class="col-sm-10">
+								<input type="Cpassword" class="form-control" id="inputPassword3" />
+							</div>
+							<div class="form-group">
+							 
+							<label for="inputAvatar3" class="col-sm-2 control-label">
+								Avatar
+							</label>
+							<div class="col-sm-offset-2 col-sm-10">
+								 
+								<button type="browse" class="btn btn-default">
+									Browse
+								</button>
+							</div>
+						</div>
+						</div>
+						</div>
+						<div class="form-group">
+							<div class="col-sm-offset-2 col-sm-10">
+								 
+								<button type="submit" class="btn btn-default">
+									Sign up
+								</button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</body>
