5625dfd997202cb460980ba67019a0313298e62a
Karim Farid <karim.f.1995@gmail.com>
2015-10-02 10:55:59 +0200

    CSS_Trial

--- signup.php
+++ signup.php
@@ -14,73 +14,26 @@
 			   	</tr>
 			</table>
 		</div>
-		<div class="container-fluid">
-			<div class="row">
-				<div class="col-md-12">
-					<form class="form-horizontal" role="form">
-						<div class="form-group">
-							 
-							<label for="inputFname3" class="col-sm-2 control-label">
-								First Name
-							</label>
-							<div class="col-sm-10">
-								<input type="fname" class="form-control" id="inputEmail3" />
+		<div  class="wrap">
+						  <div class="Regisration">
+						  	<div class="Regisration-head">
+						    	<h2><span></span>Register</h2>
+						 	 </div>
+						  	<form>
+						  		<input type="text" value="First Name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'First Name';}" >
+						  		<input type="text" value="Last Name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Last Name';}" >
+						  		<input type="text" value="Email Address" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Email Address';}" >
+								<input type="password" value="Password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Password';}" >
+								<input type="password" value=" Confirm Password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = ' Confirm Password';}" >
+								<input id="pic" type="file" name="picture" accept="image/*">
+								 <div class="Remember-me">
+
+								<div class="p-container">
+								<label class="checkbox"><input type="checkbox" name="checkbox" checked><i></i>I agree to the Terms and Conditions</label>
+								<div class ="clear"></div>
 							</div>
-						</div>
-						<label for="inputLname3" class="col-sm-2 control-label">
-								Last Name
-							</label>
-							<div class="col-sm-10">
-								<input type="lname" class="form-control" id="inputEmail3" />
-							</div>
-						</div>
-							<div class="form-group">
-							 <label for="inputEmail3" class="col-sm-2 control-label">
-								Email
-							</label>
-							<div class="col-sm-10">
-								<input type="email" class="form-control" id="inputEmail3" />
-							</div>
-							<div class="form-group">
-							 
-							<label for="inputPassword3" class="col-sm-2 control-label">
-								Password
-							</label>
-							<div class="col-sm-10">
-								<input type="password" class="form-control" id="inputPassword3" />
-							</div>
-							<div class="form-group">
-							 
-							<label for="inputCPassword3" class="col-sm-2 control-label">
-								Confirm Password
-							</label>
-							<div class="col-sm-10">
-								<input type="Cpassword" class="form-control" id="inputPassword3" />
-							</div>
-							<div class="form-group">
-							 
-							<label for="inputAvatar3" class="col-sm-2 control-label">
-								Avatar
-							</label>
-							<div class="col-sm-offset-2 col-sm-10">
-								 
-								<button type="browse" class="btn btn-default">
-									Browse
-								</button>
-							</div>
-						</div>
-						</div>
-						</div>
-						<div class="form-group">
-							<div class="col-sm-offset-2 col-sm-10">
-								 
-								<button type="submit" class="btn btn-default">
-									Sign up
-								</button>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
+												 
+								<div class="submit">
+									<input type="submit" onclick="myFunction()" value="Sign Me Up >" >
+								</div>
 	</body>
