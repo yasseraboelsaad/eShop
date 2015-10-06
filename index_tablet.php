3c02b74312efcd74c76e130eec8ff39b362cf34d
yasseraboelsaad <yasser.aboelsaad@gmail.com>
2015-10-06 15:00:28 +0200

    fixed index

--- index_tablet.php
+++ index_tablet.php
@@ -1,7 +1,5 @@
 <?php
     session_start();
-    $_SESSION['cart'] = array();
-    $_SESSION['amount'] = array();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -157,9 +155,10 @@
                                         <?php 
                                             if($row['Stock']>0){?>
                                         <form method="post" action="index.php">
-                                        <input type='submit' name=<?php echo $row['id'];?> value= "Buy me!">
-                                        <br>
-                                        <a>Number:  </a><input type="number" step="1" value="1" name=<?php echo $row['id']."no";?>>
+                                        
+            
+                                        <a>     Number: </a><input type="number" step="1" value="1" min="1"  max=<?php echo $row['Stock'];?> name=<?php echo $row['id']."no";?>>
+                                        <input type='submit' class="buy_button" name=<?php echo $row['id'];?> value= "Buy me!">
                                         </form>
                                         <?php }else{ ?>
                                         <p>Out of stock.</p>
@@ -173,7 +172,7 @@
                                             if (isset($_POST[$row['id']])) {
                                                 if (isset($_SESSION['authenticated'])) {
                                                     array_push($_SESSION['amount'],$_POST[$row['id']."no"]);
-                                                    array_push($_SESSION['cart'],$_POST[$row['id']]);
+                                                    array_push($_SESSION['cart'],$row['id']);
                                                     echo "Added to cart!";
                                                 }else{
                                                     echo "<script type='text/javascript'>alert('Please sign up or log');</script>";
