e831bf25fcc6c08c18a0a8a147bc2e9cca97c02f
Karim Farid <karim.f.1995@gmail.com>
2015-10-06 14:57:47 +0200

    Navigation Bar set in all pages.

--- cart.php
+++ cart.php
@@ -34,21 +34,17 @@
 
     <!-- Navigation -->
 
+    <!-- Navigation -->
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <img src="images/shoppingcart.png" width = "50" height = "40"> <a class="navbar-brand" href="index.php">eShop</a>
+                <img class = "navbar-brand" src="images/shoppingcart.png" style="height:50px">
+                <a class="navbar-brand" href="index.php">eShop</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
+                <ul class="nav navbar-nav navbar-right">
                     <?php if(!isset($_SESSION['user'])){ ?>
                     <li>
                         <a href="login.php">Login</a>
@@ -59,11 +55,7 @@
                         <a href="signup.php">Sign up</a>
                     </li>
                     <?php } ?>
-                    <?php if(isset($_SESSION['user'])){ ?>
-                    <li>
-                        <a href= "orders.php" >Orders</a>
-                    </li>
-                    <?php } ?>
+                    
                     <?php 
                         if(isset($_SESSION['user'])){
                             mysql_connect('localhost','root','');
@@ -75,6 +67,16 @@
                         <a href="profile.php"> 
                             <?php echo $row['Fname']." ".$row['Lname']; ?>
                         </a>
+                    </li>
+                    <?php } ?>
+                    <?php if(isset($_SESSION['user'])){ ?>
+                    <li>
+                        <a href= "cart.php" >Cart</a>
+                    </li>
+                    <?php } ?>
+                    <?php if(isset($_SESSION['user'])){ ?>
+                    <li>
+                        <a href= "orders.php" >Orders</a>
                     </li>
                     <?php } ?>
                     <?php if(isset($_SESSION['user'])){ ?>
--- profile.php
+++ profile.php
@@ -32,21 +32,17 @@
 <body>
 
     <!-- Navigation -->
+    <!-- Navigation -->
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <img src="images/shoppingcart.png" width = "50" height = "40"> <a class="navbar-brand" href="index.php">eShop</a>
+                <img class = "navbar-brand" src="images/shoppingcart.png" style="height:50px">
+                <a class="navbar-brand" href="index.php">eShop</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
+                <ul class="nav navbar-nav navbar-right">
                     <?php if(!isset($_SESSION['user'])){ ?>
                     <li>
                         <a href="login.php">Login</a>
@@ -57,14 +53,28 @@
                         <a href="signup.php">Sign up</a>
                     </li>
                     <?php } ?>
-                    <?php if(isset($_SESSION['user'])){ ?>
+                    
+                    <?php 
+                        if(isset($_SESSION['user'])){
+                            mysql_connect('localhost','root','');
+                            mysql_select_db('eshop');
+                            $sql = mysql_query("SELECT * from Users WHERE Email ='{$_SESSION['user']}'");
+                            $row=mysql_fetch_array($sql)
+                     ?>
                     <li>
-                        <a href= "orders.php" >Orders</a>
+                        <a href="profile.php"> 
+                            <?php echo $row['Fname']." ".$row['Lname']; ?>
+                        </a>
                     </li>
                     <?php } ?>
                     <?php if(isset($_SESSION['user'])){ ?>
                     <li>
                         <a href= "cart.php" >Cart</a>
+                    </li>
+                    <?php } ?>
+                    <?php if(isset($_SESSION['user'])){ ?>
+                    <li>
+                        <a href= "orders.php" >Orders</a>
                     </li>
                     <?php } ?>
                     <?php if(isset($_SESSION['user'])){ ?>
